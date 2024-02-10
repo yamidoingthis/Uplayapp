c879efc0b8986fba5b148fe57f786bec1e817b76
yamidoingthis <119778737+yamidoingthis@users.noreply.github.com>
2024-02-10 22:49:55 +0800

    home page commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { Box, Typography } from '@mui/material';
+import BannerBackground from "../img/home-banner-background.png";
+import BannerImage from "../img/happyfamily.png";
 import { Link } from 'react-router-dom';
+import { FiArrowRight } from "react-icons/fi";
 
 function Home() {
     const [itemList, setItemList] = useState([]);
@@ -38,11 +41,30 @@ function Home() {
     };
 
     return (
-        <Box>
-            <Typography variant="h5" sx={{ my: 2 }}>
-                Home
-            </Typography>
-        </Box>
+        <div className="home-container">
+            <div className="home-banner-container">
+                <div className="home-bannerImage-container">
+                    <img src={BannerBackground} alt="" />
+                </div>
+                <div className="home-text-section">
+                    <h1 className="primary-heading">
+                        You Play, We'll Do The Rest
+                    </h1>
+                    <p className="primary-text">
+
+                        More than just a booking platform, UPlay aspires to connect people from all walks of life, 
+                        forging new relationships over time as they find a common thread through shared interests. 
+                        
+                    </p>
+                    <button className="secondary-button">
+                        Book Now <FiArrowRight />{" "}
+                    </button>
+                </div>
+                <div className="home-image-section">
+                    <img src={BannerImage} alt="" />
+                </div>
+            </div>
+        </div>
     );
 }
 
