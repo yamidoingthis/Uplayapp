46e8424902e71fd003baf67436dfb30df6c2caf7
yamidoingthis <119778737+yamidoingthis@users.noreply.github.com>
2024-02-12 21:49:43 +0800

    dropdown menu for activities

--- src/pages/Activities.jsx
+++ src/pages/Activities.jsx
@@ -1,21 +1,29 @@
 import React, { useEffect, useState, useContext } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { Link } from 'react-router-dom';
-import Dropdown from 'react-bootstrap/Dropdown';
-import { Box, Typography, Grid, Card, CardContent, Input, IconButton, Button } from '@mui/material';
-import { AccountCircle, AccessTime, Search, Clear, Edit } from '@mui/icons-material';
+import { useNavigate, Link } from 'react-router-dom';
+import {
+    Box,
+    Typography,
+    Grid,
+    Card,
+    CardContent,
+    Input,
+    IconButton,
+    Button,
+    Menu,
+    MenuItem,
+} from '@mui/material';
+import { AccountCircle, AccessTime, Search, Clear, Edit, ArrowDropDown } from '@mui/icons-material';
 import http from '../http';
 import dayjs from 'dayjs';
 import UserContext from '../contexts/UserContext';
 import global from '../global';
-import ActivitiesPriceAscending from './ActivitiesPriceAscending';
-import ActivitiesPriceDescending from './ActivitiesPriceDescending';
 
 function Activities() {
     const [tutorialList, setTutorialList] = useState([]);
     const [search, setSearch] = useState('');
     const { user } = useContext(UserContext);
     const navigate = useNavigate();
+    const [sortMenuAnchor, setSortMenuAnchor] = useState(null);
 
     const onSearchChange = (e) => {
         setSearch(e.target.value);
@@ -39,18 +47,34 @@ function Activities() {
     }, []);
 
     const onSearchKeyDown = (e) => {
-        if (e.key === "Enter") {
+        if (e.key === 'Enter') {
             searchTutorials();
         }
     };
 
     const onClickSearch = () => {
         searchTutorials();
-    }
+    };
 
     const onClickClear = () => {
         setSearch('');
         getTutorials();
+    };
+
+    const openSortMenu = (event) => {
+        setSortMenuAnchor(event.currentTarget);
+    };
+
+    const closeSortMenu = () => {
+        setSortMenuAnchor(null);
+    };
+
+    const handleSortOptionClick = (sortOption) => {
+        
+        console.log('Selected sort option:', sortOption);
+
+        // Close the menu
+        closeSortMenu();
     };
 
     return (
@@ -60,108 +84,108 @@ function Activities() {
             </Typography>
 
             <Box sx={{ display: 'flex', alignItems: 'center', mb: 2 }}>
-                <Input value={search} placeholder="Search"
+                <Input
+                    value={search}
+                    placeholder="Search"
                     onChange={onSearchChange}
-                    onKeyDown={onSearchKeyDown} />
-                <IconButton color="primary"
-                    onClick={onClickSearch}>
+                    onKeyDown={onSearchKeyDown}
+                />
+                <IconButton color="primary" onClick={onClickSearch}>
                     <Search />
                 </IconButton>
-                <IconButton color="primary"
-                    onClick={onClickClear}>
+                <IconButton color="primary" onClick={onClickClear}>
                     <Clear />
                 </IconButton>
-                <Box sx={{ flexGrow: 1 }} />
-                <Dropdown>
-                    <Dropdown.Toggle variant="success" id="dropdown-basic">
-                        Sort By
-                    </Dropdown.Toggle>
-
-                    <Dropdown.Menu>
-                        <Dropdown.Item><Link to="/activities">Recently Added</Link></Dropdown.Item>
-                        <Dropdown.Item><Link to="/activitiespricedescending">Most Expensive First</Link></Dropdown.Item>
-                        <Dropdown.Item><Link to="/activitiespriceascending">Cheapest First</Link></Dropdown.Item>
-                    </Dropdown.Menu>
-                </Dropdown>
-                {
-                    user && (
-                        <Link to="/addactivity" style={{ textDecoration: 'none' }}>
-                            <Button variant='contained'>
-                                Add
-                            </Button>
-                        </Link>
-                    )
-                }
+                <Box sx={{ flexGrow: 1, }} />
+                <Button
+                    variant="contained"
+                    endIcon={<ArrowDropDown />}
+                    onClick={openSortMenu}
+                    sx={{ marginRight: 2}}
+                >
+                    Sort By
+                </Button>
+                <Menu
+                    anchorEl={sortMenuAnchor}
+                    open={Boolean(sortMenuAnchor)}
+                    onClose={closeSortMenu}
+                >
+                    <MenuItem onClick={() => handleSortOptionClick('recentlyAdded')}>
+                        <Link to="/activities" style={{ textDecoration: 'none' , color: "black"  }}>Recently Added</Link>
+                    </MenuItem>
+                    <MenuItem onClick={() => handleSortOptionClick('mostExpensiveFirst')}>
+                        <Link to="/activitiespricedescending" style={{ textDecoration: 'none', color: "black" }}>Most Expensive First</Link>
+                    </MenuItem>
+                    <MenuItem onClick={() => handleSortOptionClick('cheapestFirst')}>
+                        <Link to="/activitiespriceascending" style={{ textDecoration: 'none',  color: "black"  }}>Cheapest First</Link>
+                    </MenuItem>
+                </Menu>
+                {user && (
+                    <Link to="/addactivity" style={{ textDecoration: 'none' }}>
+                        <Button variant="contained">Add</Button>
+                    </Link>
+                )}
             </Box>
 
             <Grid container spacing={2}>
-                {
-                    tutorialList.map((tutorial, i) => {
-                        return (
-                            <Grid item xs={12} md={6} lg={4} key={tutorial.id}>
-                                <Card>
-                                    {
-                                        tutorial.imageFile && (
-                                            <Box className="aspect-ratio-container">
-                                                <img alt="tutorial"
-                                                    src={`${import.meta.env.VITE_FILE_BASE_URL}${tutorial.imageFile}`}>
-                                                </img>
-                                            </Box>
-                                        )
-                                    }
-                                    <CardContent>
-                                        <Box sx={{ display: 'flex', mb: 1 }}>
-                                            <Typography variant="h6" sx={{ flexGrow: 1 }}>
-                                                {tutorial.name}
-                                            </Typography>
-                                            {
-                                                user && user.id === tutorial.vendorId && (
-                                                    <Link to={`/editactivity/${tutorial.id}`}>
-                                                        <IconButton color="primary" sx={{ padding: '4px' }}>
-                                                            <Edit />
-                                                        </IconButton>
-                                                    </Link>
-                                                )
-                                            }
-                                        </Box>
-                                        <Box sx={{ display: 'flex', alignItems: 'center', mb: 1 }}
-                                            color="text.secondary">
-                                            <AccountCircle sx={{ mr: 1 }} />
-                                            <Typography>
-                                                {tutorial.vendor?.name}
-                                            </Typography>
-                                        </Box>
-                                        <Box sx={{ display: 'flex', alignItems: 'center', mb: 1 }}
-                                            color="text.secondary">
-                                            <AccessTime sx={{ mr: 1 }} />
-                                            <Typography>
-                                                {dayjs(tutorial.activityDate).format(global.datetimeFormat)}
-                                            </Typography>
-                                        </Box>
-                                        <Typography sx={{ whiteSpace: 'pre-wrap' }}>
-                                            Price: ${tutorial.price}
+                {tutorialList.map((tutorial, i) => {
+                    return (
+                        <Grid item xs={12} md={6} lg={4} key={tutorial.id}>
+                            <Card>
+                                {tutorial.imageFile && (
+                                    <Box className="aspect-ratio-container">
+                                        <img
+                                            alt="tutorial"
+                                            src={`${import.meta.env.VITE_FILE_BASE_URL}${tutorial.imageFile}`}
+                                        />
+                                    </Box>
+                                )}
+                                <CardContent>
+                                    <Box sx={{ display: 'flex', mb: 1 }}>
+                                        <Typography variant="h6" sx={{ flexGrow: 1 }}>
+                                            {tutorial.name}
                                         </Typography>
-                                        <Link to={`/activity/${tutorial.id}`}>
-                                            <Typography>
-                                                View More
-                                            </Typography>
-                                        </Link>
-                                        <Link to={`/reviews/${tutorial.id}`}>
-                                            <Typography>
-                                                View Reviews
-                                            </Typography>
-                                        </Link>
-                                        <Link to={`/addreview/${tutorial.id}`}>
-                                            <Typography>
-                                                Add Review
-                                            </Typography>
-                                        </Link>
-                                    </CardContent>
-                                </Card>
-                            </Grid>
-                        );
-                    })
-                }
+                                        {user && user.id === tutorial.vendorId && (
+                                            <Link to={`/editactivity/${tutorial.id}`}>
+                                                <IconButton color="primary" sx={{ padding: '4px' }}>
+                                                    <Edit />
+                                                </IconButton>
+                                            </Link>
+                                        )}
+                                    </Box>
+                                    <Box
+                                        sx={{ display: 'flex', alignItems: 'center', mb: 1 }}
+                                        color="text.secondary"
+                                    >
+                                        <AccountCircle sx={{ mr: 1 }} />
+                                        <Typography>{tutorial.vendor?.name}</Typography>
+                                    </Box>
+                                    <Box
+                                        sx={{ display: 'flex', alignItems: 'center', mb: 1 }}
+                                        color="text.secondary"
+                                    >
+                                        <AccessTime sx={{ mr: 1 }} />
+                                        <Typography>
+                                            {dayjs(tutorial.activityDate).format(global.datetimeFormat)}
+                                        </Typography>
+                                    </Box>
+                                    <Typography sx={{ whiteSpace: 'pre-wrap' }}>
+                                        Price: ${tutorial.price}
+                                    </Typography>
+                                    <Link to={`/activity/${tutorial.id}`}>
+                                        <Typography>View More</Typography>
+                                    </Link>
+                                    <Link to={`/reviews/${tutorial.id}`}>
+                                        <Typography>View Reviews</Typography>
+                                    </Link>
+                                    <Link to={`/addreview/${tutorial.id}`}>
+                                        <Typography>Add Review</Typography>
+                                    </Link>
+                                </CardContent>
+                            </Card>
+                        </Grid>
+                    );
+                })}
             </Grid>
         </Box>
     );
