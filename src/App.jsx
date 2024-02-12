ccd9553fb2513fefdd2451d21e06a5582a507efe
yamidoingthis <119778737+yamidoingthis@users.noreply.github.com>
2024-02-12 17:34:37 +0800

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -97,7 +97,6 @@ function App() {
                     <Box sx={{ flexGrow: 1 }}></Box>
                     <Link to="/home" ><Typography sx={{ mr: 2 }}>Home</Typography></Link>
                     <Link to="/activities" ><Typography sx={{ mr: 2 }}>Activities</Typography></Link>
-                    <Link to="/bookings" ><Typography sx={{ mr: 2 }}>Bookings</Typography></Link>
                     <Link to="/cart" ><Typography sx={{ mr: 2 }}>Your Cart</Typography></Link>
                   </>
                   )}
